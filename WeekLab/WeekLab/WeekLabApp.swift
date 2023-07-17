c86b96c9ba9a49f86b30a1192306c6d9693121c1
Nikon Parvitskiy <Nikon.ser.par@gmail.com>
2023-07-18 02:13:15 +0300

    init commit
    first commit

---
+++ WeekLab/WeekLab/WeekLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekLabApp.swift
+//  WeekLab
+//
+//  Created by Nikon Parvitskiy on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct WeekLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
